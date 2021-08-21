1281d1b7bb7cb7fbe279bc3f9bae2da53ca76db5
MPult <MPult@protonmail.com>
2021-08-21 20:57:24 +0200

    fixed fucksi wupsi in version route; env variables didn'twork

--- src/routes/version.ts
+++ src/routes/version.ts
@@ -1,10 +1,15 @@
 module.exports = async function (app, environment) {
   const express = require('express');
+  const enviornment = require('../middleware/environment')
+  const env = enviornment.default
   // Middleware
 
   app.get('/version', (req, res) => {
+    console.log(env);
     res.status(200).json({
-      v: environment.SERVICE_VERSION,
+      serviceName: env.SERVICE_VERSION,
+      servicePort: env.SERVICE_PORT,
+      serviceVersion: env.SERVICE_VERSION,
     });
   });
 
